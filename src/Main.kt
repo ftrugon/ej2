34bcf06520d7ef9ab6701c4e37323e26b57c7bce
franconil <franconil04@gmail.com>
2024-01-27 18:10:17 +0100

    ej2 comentado

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,21 @@
 import kotlin.math.roundToInt
-
+/**
+ * Clase que representa una persona con propiedades como peso, altura y nombre.
+ *
+ * @property peso El peso de la persona.
+ * @property altura La altura de la persona.
+ * @property nombre El nombre de la persona.
+ * @property imc El índice de masa corporal (IMC) de la persona.
+ *
+ * @constructor Crea un objeto [Persona] con el peso y la altura especificados.
+ * @param peso El peso de la persona.
+ * @param altura La altura de la persona.
+ *
+ * @constructor Crea un objeto [Persona] con el peso, la altura y el nombre especificados.
+ * @param peso El peso de la persona.
+ * @param altura La altura de la persona.
+ * @param nombre El nombre de la persona.
+ */
 class Persona(
     val peso:Double,
     var altura:Double
@@ -10,21 +26,36 @@ class Persona(
             field = value
         }
 
+    /**
+     * El índice de masa corporal (IMC) de la persona.
+     */
     var imc = peso / (altura*2)
 
+    /**
+     * Constructor secundario que permite proporcionar también el nombre al crear una persona.
+     */
     constructor(peso: Double,altura: Double,nombre:String):this(peso,altura){
         this.nombre = nombre
     }
 
-
+    /**
+     * Representación en cadena del objeto [Persona].
+     *
+     * @return Una cadena que representa el peso, la altura, el IMC y el nombre de la persona.
+     */
     override fun toString(): String {
         return """Persona: 
- peso: $peso
- altura: ${String.format("%.2f",this.imc)}
- imc: $imc 
- nombre: $nombre"""
+     peso: $peso
+     altura: ${String.format("%.2f",this.imc)}
+     imc: $imc 
+     nombre: $nombre"""
     }
 
+    /**
+     * Compara si dos objetos [Persona] son iguales.
+     *
+     * @return `true` si los objetos son iguales, `false` en caso contrario.
+     */
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other !is Persona) return false
@@ -36,6 +67,9 @@ class Persona(
     }
 }
 
+/**
+ * Función principal que interactúa con objetos [Persona] y los imprime.
+ */
 fun main() {
     print("DiMe Tu NoMbRe: ")
     val nombre = readln()
